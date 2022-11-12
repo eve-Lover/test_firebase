9753c292ccf720303c1cedf8c8d23e6d294c6dc7
kimNaYoung <nayy6947@gmail.com>
2022-11-12 17:23:49 +0900

    tab1 테스트 -1

--- lib/tab1.dart
+++ lib/tab1.dart
@@ -1,1 +1,19 @@
 // tab1 - 나영
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(
+      home: MyApp()
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("테스트"),
+
+    );
+  }
+}
