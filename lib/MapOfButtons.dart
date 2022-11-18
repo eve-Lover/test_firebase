d68ce1ceddfba8a2eb704b259fabd083ac9a86a2
kimNaYoung <nayy6947@gmail.com>
2022-11-18 23:12:03 +0900

    마이페이지 풍경 버튼 테스트

---
+++ lib/MapOfButtons.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+var buttons1 = [
+  {
+    'text' : '산',
+    'isSelectd' : false,
+    'icon' : const Icon(Icons.landscape, color: Colors.black, size: 50.0,),
+    'color' : Colors.white
+  },
+  {
+    'text' : '바다',
+    'isSelectd' : false,
+    'icon' : const Icon(Icons.landscape, color: Colors.black, size: 50.0,),
+    'color' :Colors.white
+  },
+  {
+    'text' :  '오름',
+    'isSelectd' : false,
+    'icon' : const Icon(Icons.beach_access, color: Colors.black, size: 50.0,),
+    'color' :Colors.white
+  },
+  {
+    'text' :  '시내',
+    'isSelectd' : false,
+    'icon' : const Icon(Icons.location_city, color: Colors.black, size: 50.0,),
+    'color' :Colors.white
+  },
+  {
+    'text' :  '시골',
+    'isSelectd' : false,
+    'icon' : const Icon(Icons.beach_access, color: Colors.black, size: 50.0,),
+    'color' :Colors.white
+  }
+];
+/*
+change(button){
+  if(button['isSelected'] == false){
+    setState((){
+      colors[i] = Colors.pinkAccent;
+      isSelected[i] = true;
+    });
+  }else{
+    setState(() {
+      colors[i] = Colors.white;
+      isSelected[i] = false;
+    });
+  }
+}*/
--- lib/Mypage.dart
+++ lib/Mypage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:circle_button/circle_button.dart';
 // 나영 작업중
 
 class MyPage extends StatefulWidget {
@@ -9,10 +10,60 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
+  // Color color = Colors.white;
+  List<String> typeOfScenery = ['산', '바다', '오름', '시내','시골'];
+  List<Icon> icons = [
+    const Icon(Icons.landscape, color: Colors.black, size: 50.0,),
+    const Icon(Icons.beach_access, color: Colors.black, size: 50.0,),
+    const Icon(Icons.eject, color: Colors.black, size: 50.0,),
+    const Icon(Icons.location_city,color: Colors.black, size: 50.0, ),
+    const Icon(Icons.nature,color: Colors.black, size: 50.0,)
+  ];
+  List<bool> isSelected = List<bool>.filled(5, true);
+  List<Color> colors = List<Color>.filled(5, Colors.white);
+
+  changeColor(int i) {
+    if(isSelected[i] == false){
+      setState((){
+        colors[i] = Colors.pinkAccent;
+        isSelected[i] = true;
+      });
+    }else{
+      setState(() {
+        colors[i] = Colors.white;
+        isSelected[i] = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('마이페이지')
+      body:  ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: typeOfScenery.length,
+        itemBuilder: (c,i){
+          return CircleButton( // 산
+            onTap: () => changeColor(i),
+            tooltip: 'Circle Button',
+            width: 100.0,
+            height: 100.0,
+            borderColor: Colors.grey,
+            borderWidth: 2.0,
+            borderStyle: BorderStyle.solid,
+            backgroundColor: colors[i],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icons[i],
+                Text(typeOfScenery[i])
+              ],
+            ),
+          );
+        },
+      ),
+
+
     );
   }
 }
